9aeefb7f041892e14cfbc50bba3b48ab34244e81
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-08-12 00:14:26 -0700

    Add starting files

---
+++ Click Counter/Click Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Kenny Iraheta on 8/12/17.
+//  Copyright © 2017 KennyIraheta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
