1da121c935378c23449d52fbe319290dd4712baa
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-08-14 17:57:48 -0700

    Transiitioning to Storyboard

--- Click Counter/Click Counter/ViewController.swift
+++ Click Counter/Click Counter/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+    
+    @IBAction func incrementCount() {
+        self.count = self.count + 1
+        self.label.text = "\(self.count)"
     }
 
 }
